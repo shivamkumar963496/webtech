09a9b85649ed656a9f7d9d819a3b777114d16ccb
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-01-09 19:40:57 +0530

    Update cp5.c

--- linkhub/c-programs/function/cp5.c
+++ linkhub/c-programs/function/cp5.c
@@ -1,18 +1,28 @@
-/*Maximum of Two Numbers*/
+/* Maximum of Two Numbers
+   Ye program function ka use karke
+   do numbers me se bada number find karta hai
+*/
 
-#include <stdio.h>
+#include <stdio.h>   // Input-output ke liye header file
 
-// दो numbers में से बड़ा number return करने वाला function
+// Ye function do numbers leta hai
+// Aur unme se jo bada hota hai, wahi return karta hai
 int max(int a, int b)
 {
+    // Agar a, b se bada hai
     if(a > b)
-        return a;
+        return a;   // to a return karo
     else
-        return b;
+        return b;   // warna b return karo
 }
 
 int main()
 {
+    // max(15, 25) function call kar rahe hain
+    // Jo bhi bada number hoga, wo print hoga
     printf("Maximum = %d", max(15, 25));
+
+    // Program yahin end hota hai
     return 0;
 }
+
